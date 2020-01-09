347f152823a69253e6f8d8d6d62834031c7c3a60
Kiryl Balanovich <balanovichks@gmail.com>
2020-01-09 16:05:58 +0300

    Max number added

---
+++ MaxNum/MaxNum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MaxNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number ");
+            double firstNum = double.Parse(Console.ReadLine());            
+            Console.WriteLine("Enter second number ");
+            double secondNum = double.Parse(Console.ReadLine());
+            double maxNum = firstNum >= secondNum ? firstNum : secondNum;
+            Console.WriteLine($"Max number is {maxNum}");
+        }
+    }
+}
