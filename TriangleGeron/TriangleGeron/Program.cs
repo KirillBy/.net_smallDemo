cf345831edd58acb37ce60a0c082995f133278b4
Kiryl Balanovich <balanovichks@gmail.com>
2020-01-09 14:41:39 +0300

    Triangle Geron Formula added

---
+++ TriangleGeron/TriangleGeron/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TriangleGeron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("Please enter first line  of triangle: ");
+            double.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Please enter second line  of triangle: ");
+            double.TryParse(Console.ReadLine(), out b);
+            Console.WriteLine("Please enter third line  of triangle: ");
+            double.TryParse(Console.ReadLine(), out c);
+
+            double halfPer = (a + b + c) / 2;
+            double square = Math.Sqrt(halfPer * (halfPer - a) * (halfPer - b) * (halfPer - c));
+
+            Console.WriteLine($"Square of triangle is {square}");
+
+        }
+    }
+}
