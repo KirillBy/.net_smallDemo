ac51cf1d3e08250163ffe46442399f5364e670c5
Kiryl Balanovich <balanovichks@gmail.com>
2020-01-10 20:58:01 +0300

    triangle square calculate reload added

---
+++ TriangleSquareReload/TriangleSquareReload/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TriangleSquareReload;
+namespace TriangleSquareReload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("square with three sides is");
+            Console.WriteLine($"{TriangleSquare.CalculateSquare(2, 3, 4)}");
+            Console.WriteLine("square with hight and base is");
+            Console.WriteLine($"{TriangleSquare.CalculateSquare(4, 5)}");
+            Console.WriteLine("square with two sides and angle");
+            Console.WriteLine($"{TriangleSquare.CalculateSquare(4, 5, 60)}");
+        }
+    }
+}
---
+++ TriangleSquareReload/TriangleSquareReload/TriangleSquare.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TriangleSquareReload
+{
+    class TriangleSquare
+    {
+        public static double CalculateSquare(double ab, double bc, double ca)
+        {
+            double halfPer = (ab + bc + ca) / 2;
+            return Math.Sqrt(halfPer * (halfPer - ab) * (halfPer - bc) * (halfPer - ca));
+        }
+        public static double CalculateSquare(double h, double b)
+        {
+            return  (h * b)/2;
+        }
+        public static double CalculateSquare(double ab, double bc, int alpha)
+        {
+            double rads = alpha * Math.PI / 180;
+            return (ab * bc * Math.Sin(rads)) / 2;
+        }
+    }
+}
