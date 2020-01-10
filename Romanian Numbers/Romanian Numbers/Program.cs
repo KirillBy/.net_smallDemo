ee9f86889fb8735a6ffdf09d12ec83613726ada6
Kiryl Balanovich <balanovichks@gmail.com>
2020-01-10 15:17:13 +0300

    Romanian Numbers added

---
+++ Romanian Numbers/Romanian Numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Romanian_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RomanianNumber.Parse("IV"));
+        }  
+    }
+}
