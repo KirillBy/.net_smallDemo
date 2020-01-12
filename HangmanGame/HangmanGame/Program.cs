3193b9614cc293461c880bbcee2bc51c9f56d2cd
Kiryl Balanovich <balanovichks@gmail.com>
2020-01-12 20:01:01 +0300

    Hangman Game added

---
+++ HangmanGame/HangmanGame/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HangmanGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HangmanGame game = new HangmanGame();
+            string word = game.GenerateWord();
+            Console.WriteLine($"The word consist of {word.Length} letters.");
+            Console.WriteLine("Try to guess the word.");
+            while(game.GameStatus == GameStatus.InProgress)
+            {
+                Console.WriteLine("Pick a letter");
+                char c = Console.ReadLine().ToCharArray()[0];
+
+                string curState = game.GuessLetter(c);
+                Console.WriteLine(curState);
+
+                Console.WriteLine($"Remaining tries = {game.RemainingTries}");
+                Console.WriteLine($"Tried letters: {game.TriedLetters}");
+            }
+            if(game.GameStatus == GameStatus.Lost)
+            {
+                Console.WriteLine("Your are hanged.");
+                Console.WriteLine($"The word was {game.Word}");
+            }
+             else if(game.GameStatus == GameStatus.Won)
+            {
+                Console.WriteLine("You won!");
+            }
+        }
+    }
+}
