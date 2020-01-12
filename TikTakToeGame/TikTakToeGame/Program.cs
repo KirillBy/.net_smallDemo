676c7c79a5b55866e90110208c5b3cd0eb63ecf2
Kiryl Balanovich <balanovichks@gmail.com>
2020-01-12 18:44:22 +0300

    TikTakToe game added

---
+++ TikTakToeGame/TikTakToeGame/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace TikTakToeGame
+{
+    class Program
+    {
+        private static TikTakToe g = new TikTakToe();
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPrintableState());
+            while(g.GetWinner() == Winner.GameIsUnfinished)
+            {
+                if(g.MovesCounter%2 == 0)
+                    Console.WriteLine("X player turn");
+                else
+                    Console.WriteLine("O player turn");
+                int index = int.Parse(Console.ReadLine());
+                g.MakeMove(index);
+                Console.WriteLine();
+                Console.Clear();
+                Console.WriteLine(GetPrintableState());
+            }
+            Console.WriteLine($"Result: {g.GetWinner()} win");
+            
+        }
+        static string GetPrintableState()
+        {
+            var sb = new StringBuilder();
+            for (int i = 1; i <= 7; i+=3)
+            {
+                sb.AppendLine("     |     |     |").AppendLine(
+                    $"  {GetPrintableChar(i)}  |  {GetPrintableChar(i + 1)}  |  {GetPrintableChar(i + 2)}  |")
+                    .AppendLine("_____|_____|_____|");
+            }
+            return sb.ToString();
+        }
+        static string GetPrintableChar(int index)
+        {
+            State state = g.GetState(index);
+            if (state == State.Unset)
+                return index.ToString();
+            return state == State.Cross ? "X" : "O";
+        }
+    }
+}
