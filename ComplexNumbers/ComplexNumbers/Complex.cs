c9fe73a87f5f3a628814515ce247a07451ed2edb
Kiryl Balanovich <balanovichks@gmail.com>
2020-01-12 13:39:38 +0300

    complex numbers addes

---
+++ ComplexNumbers/ComplexNumbers/Complex.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComplexNumbers
+{
+    public class Complex
+    {
+        private long materialPart;
+        public long MaterialPart
+        {
+            get { return materialPart; }
+            set { materialPart = value; }
+        }
+
+        private long floatPart;
+        public long FloatPart
+        {
+            get { return floatPart; }
+            set { floatPart = value; }
+        }
+        public Complex(long materialPart,  long floatPart)
+        {
+            MaterialPart = materialPart;
+            FloatPart = floatPart;
+        }
+
+        public Complex Plus(Complex numb)
+        {
+            Complex result = new Complex(this.materialPart, this.floatPart);
+            result.materialPart += numb.materialPart;
+            result.floatPart += numb.floatPart;
+
+            return result;
+        }
+        public Complex Minus(Complex numb)
+        {
+            Complex result = new Complex(this.materialPart, this.floatPart);
+            result.materialPart -= numb.materialPart;
+            result.floatPart -= numb.floatPart;
+
+            return result;
+        }
+
+
+
+    }
+}
---
+++ ComplexNumbers/ComplexNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ComplexNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex(2,9);
+            Complex c2 = new Complex(1,2);
+            Complex result = c1.Plus(c2);
+            Console.WriteLine($"{result.MaterialPart}.{result.FloatPart}");
+            result = c1.Minus(c2);
+            Console.WriteLine($"{result.MaterialPart}.{result.FloatPart}");
+        }
+    }
+}
