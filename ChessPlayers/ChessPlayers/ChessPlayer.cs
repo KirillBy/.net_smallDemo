9c56f722776b7f0610bbb47a672b961878368444
Kiryl Balanovich <balanovichks@gmail.com>
2020-01-14 10:09:55 +0300

    Chess Player find rus added

---
+++ ChessPlayers/ChessPlayers/ChessPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessPlayers
+{
+    public class ChessPlayer
+    {
+        public string Country { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BirthYear { get; set; }
+        public int Rating { get; set; }
+        public int Id { get; set; }
+
+        public override string ToString()
+        {
+            return $"Full Name: {FirstName + " " + LastName}, Rating = {Rating}, from {Country}, born in {BirthYear}";
+        }
+
+        public static ChessPlayer ParseFideCsvLine(string line)
+        {
+            string[] parts = line.Split(';');
+            return new ChessPlayer()
+            {
+                Id = int.Parse(parts[0]),
+                LastName = parts[1].Split(',')[0].Trim(),
+                FirstName = parts[1].Split(',')[1].Trim(),
+                Country = parts[3],
+                Rating = int.Parse(parts[4]),
+                BirthYear = int.Parse(parts[6])
+            };
+        }
+    }
+}
---
+++ ChessPlayers/ChessPlayers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ChessPlayers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HW_FindRusChessPlayers();
+        }
+        static void HW_FindRusChessPlayers()
+        {
+            var players = File.ReadAllLines(@"Top100ChessPlayers.csv")
+                              .Skip(1)
+                              .Select(ChessPlayer.ParseFideCsvLine)
+                              .Where(player => player.Country == "RUS")
+                              .OrderBy(player => player.BirthYear)
+                              .ToList();
+
+            foreach (var player in players)
+            {
+                Console.WriteLine(player);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
