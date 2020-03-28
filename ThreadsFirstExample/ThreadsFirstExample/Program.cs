8970e642ba060c6f2d0fa70101d73689894f40c6
Kiryl Balanovich <balanovichks@gmail.com>
2020-03-28 13:20:30 +0300

    Thread first example demo

---
+++ ThreadsFirstExample/ThreadsFirstExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadsFirstExample
+{
+    class MyThread
+    {
+        public int Count;
+        string thrdName;
+        public MyThread(string name)
+        {
+            Count = 0;
+            thrdName = name;
+        }
+        public void Run()
+        {
+            Console.WriteLine("Thread " + thrdName + " has begun" );
+            do
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("In thread " + thrdName + " count is " + Count);
+                Count++;
+            } while (Count < 10);
+            Console.WriteLine("Thread " + thrdName + " is finished");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread has begun");
+            MyThread mt = new MyThread("Thread No 1");
+            Thread newThrd = new Thread(mt.Run);
+            newThrd.Start();
+            do
+            {
+                Console.WriteLine(".");
+                Thread.Sleep(100);
+            }while(mt.Count != 10);
+        }
+    }
+}
