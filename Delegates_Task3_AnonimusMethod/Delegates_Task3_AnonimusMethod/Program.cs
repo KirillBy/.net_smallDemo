b1dab582cb97d71a6f83c69f20da4f1874fafd61
Kiryl Balanovich <balanovichks@gmail.com>
2020-04-08 17:42:13 +0300

    Delegate Task 3 Anonimus Method

---
+++ Delegates_Task3_AnonimusMethod/Delegates_Task3_AnonimusMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Delegates_Task3_AnonimusMethod
+{
+    class Program
+    {
+        public delegate int mydelegate();
+        public delegate int mydel(mydelegate[] arr);
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            mydelegate[] delegateArray = new mydelegate[5];
+            for (int i = 0; i < delegateArray.Length; i++)
+                delegateArray[i] = () => rand.Next(0, 1000);
+            mydel md = delegate (mydelegate[] arr)
+            {
+                int count = 0;
+                for (int i = 0; i < arr.Length; i++)
+                { count += arr[i].Invoke(); Console.WriteLine(count); }
+                return count / arr.Length;
+            };
+            Console.WriteLine(md.Invoke(delegateArray));
+        }
+    }
+}
