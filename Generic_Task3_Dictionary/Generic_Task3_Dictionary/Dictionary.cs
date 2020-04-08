700fadc6d15b9b859f2bfc531c2cf67cebdd2f87
Kiryl Balanovich <balanovichks@gmail.com>
2020-04-08 16:31:56 +0300

    Generic Task3 Dictionary

---
+++ Generic_Task3_Dictionary/Generic_Task3_Dictionary/Dictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Task3_Dictionary
+{
+    class Dictionary<TKey, TValue>
+    {
+        private const int LIMIT = 4;
+        private int limitControl = 0;
+        private TKey[] key = new TKey[5];
+        private TValue[] value = new TValue[5];
+
+        public void Add(TKey key, TValue value)
+        {
+            if (limitControl <= LIMIT)
+            {
+                this.key[limitControl] = key;
+                this.value[limitControl] = value;
+                limitControl++;
+            }
+            else
+                Console.WriteLine("No more place in Dictionary");
+                
+        }
+        public string this[TKey index]
+        {
+            get
+            {
+                for (int i = 0; i < key.Length; i++)
+                    if (key[i].Equals(index))
+                        return key[i] + " - " + value[i];
+
+                return string.Format("{0} - нет перевода для этого слова.", index);
+            }
+        }
+
+        public string this[TValue index]
+        {
+            get
+            {
+                for (int i = 0; i < value.Length; i++)
+                    if (value[i].Equals(index))
+                        return key[i] + " - " + value[i];
+
+                return string.Format("{0} - нет перевода для этого слова.", index);
+            }
+        }
+    }
+}
---
+++ Generic_Task3_Dictionary/Generic_Task3_Dictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Generic_Task3_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(1, "Asus");
+            dictionary.Add(2, "Acer");
+            dictionary.Add(3, "MSI");
+            Console.WriteLine(dictionary[1] + " " + dictionary[1]);
+            Console.WriteLine(dictionary[3] + " " + dictionary[2]);
+            Console.WriteLine(dictionary[3] + " " + dictionary[3]);
+            dictionary.Add(4, "HP");
+            dictionary.Add(5, "DELL");
+            dictionary.Add(6, "Sony");
+
+
+        }
+    }
+}
