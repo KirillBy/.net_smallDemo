47546a5eacac7d7edbf79de4e07bacb4b37c9629
Kiryl Balanovich <balanovichks@gmail.com>
2020-04-08 16:46:58 +0300

    Generic ExtraTask FactoryMethod

---
+++ Generic_ExtraTask_FactoryMethod/Generic_ExtraTask_FactoryMethod/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_ExtraTask_FactoryMethod
+{
+    class MyClass<T> where T: new()
+    {
+         public T FactoryMethod() => new T();
+    }
+}
---
+++ Generic_ExtraTask_FactoryMethod/Generic_ExtraTask_FactoryMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Generic_ExtraTask_FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<int> intClass = new MyClass<int>();
+            MyClass<double> doubleClass = new MyClass<double>();
+            var unknown1 = intClass.FactoryMethod();
+            Console.WriteLine(unknown1.GetType());
+            var unknown2 = doubleClass.FactoryMethod();
+            Console.WriteLine(unknown2.GetType());
+        }
+    }
+}
