c9c7c0faa9cb1b572556212e8245af1b8c5b1507
Kiryl Balanovich <balanovichks@gmail.com>
2020-04-08 15:49:49 +0300

    Generic Task2 MyList

---
+++ Generics_Task2_MyList/Generics_Task2_MyList/MyList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics_Task2_MyList
+{
+     class MyList<T>
+    {
+        private T[] myArray;
+
+        //property for showing lenght
+        public int Lenght { get { if (myArray == null)
+                                   return 0;
+                                  return myArray.Length; }  }
+
+        //indexer
+        public object this[int index]
+        {
+            get { if (index >= 0 && index < myArray.Length) { return myArray[index]; }
+                else { Console.WriteLine("Index out of range"); return myArray[myArray.Length - 1]; } } 
+            set { if (index >= 0 && index < myArray.Length) { myArray[index] = (T)value; }
+                else { Console.WriteLine("Index out of range"); }
+            }
+        }
+
+        //add elements method
+        public void Add(T newelement)
+        {
+            if (myArray != null)
+            {
+                T[] timearr = myArray;
+                int size = timearr.Length;
+                myArray = new T[size + 1];
+                for (int i = 0; i < timearr.Length; i++)
+                {
+                    myArray[i] = timearr[i];
+                }
+                myArray[size] = newelement;
+            }
+            else
+                myArray = new T[1] { newelement };
+        }
+
+
+        // Display all elements method
+        public void DisplayAllElements()
+        {
+            if(myArray != null)
+            {
+                foreach (var item in myArray)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+                Console.WriteLine("List is empty");
+        
+        }
+    }
+}
---
+++ Generics_Task2_MyList/Generics_Task2_MyList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Generics_Task2_MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            Console.WriteLine("Lenght now " + myList.Lenght);
+            myList.DisplayAllElements();
+            myList.Add(5);
+            myList.DisplayAllElements();
+            Console.WriteLine("Lenght now " + myList.Lenght);
+            myList.Add(100);
+            myList.Add(111);
+            myList.Add(222);
+            myList.Add(112);
+            myList.DisplayAllElements();
+            myList[1] = 101;
+            Console.WriteLine(myList[1]);
+            myList[10] = 400;
+        }
+    }
+}
