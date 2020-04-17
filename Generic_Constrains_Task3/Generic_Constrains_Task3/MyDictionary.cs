321b58e5f0bc35d6b6c499d7b51dd3141c8aba3b
Kiryl Balanovich <balanovichks@gmail.com>
2020-04-17 12:02:52 +0300

    Generics Constrains Task3 DictionaryClone

---
+++ Generic_Constrains_Task3/Generic_Constrains_Task3/MyDictionary.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Constrains_Task3
+{
+    internal class MyDictionary<TKey, TValue> where TKey: IEquatable<TKey>
+    {
+
+        TKey[] keyArray;
+        TValue[] valueArray;
+
+        //Lenght property
+        public int Lenght { get { return keyArray.Length; }  }
+
+
+        //indexer
+        public TValue this[TKey index]
+        {
+            get 
+            {
+                int returnIndex = -1;
+                for(int i = 0 ; i < keyArray.Length; i++)
+                {
+                    if (keyArray[i].Equals(index))
+                        returnIndex = i;
+                }
+                if (returnIndex >= 0)
+                    return valueArray[returnIndex];
+                else
+                    return valueArray[valueArray.Length-1];
+            }
+           
+        }
+        // add method
+        public void Add(TKey key, TValue value)
+        {
+            if( keyArray == null)
+            { 
+                keyArray = new TKey[] { key };
+                valueArray = new TValue[] { value };
+            }
+            else
+            {
+                TKey[] keyTemp = keyArray;
+                TValue[] valueTemp = valueArray;
+                keyArray = new TKey[keyTemp.Length + 1];
+                valueArray = new TValue[valueTemp.Length + 1];
+                for (int i = 0; i < keyTemp.Length; i++)
+                {
+                    keyArray[i] = keyTemp[i];
+                    valueArray[i] = valueTemp[i];
+                }
+                keyArray[keyTemp.Length] = key;
+                valueArray[valueTemp.Length] = value;
+            }
+        }
+        //remove method
+        public void Remove(int index)
+        {
+
+            if (keyArray == null)
+            {
+                Console.WriteLine("Your dictionary is already empty");
+            }
+            else
+            {
+
+                int indexToDelete = -1;
+                for(int i = 0; i < keyArray.Length; i++)
+                {
+                    if (index.Equals(keyArray[i]))
+                        indexToDelete = index;
+                }
+                if(indexToDelete > 0)
+                {
+                    TKey[] keyTemp = keyArray;
+                    TValue[] valueTemp = valueArray;
+                    keyArray = new TKey[keyTemp.Length - 1];
+                    valueArray = new TValue[valueTemp.Length - 1];
+                    int k = 0;
+                    for (int i = 0; i < keyTemp.Length; i++)
+                    {
+                        if(!indexToDelete.Equals(keyTemp[i]))
+                        {
+                            keyArray[k] = keyTemp[i];
+                            valueArray[k] = valueTemp[i];
+                            k++;
+                        }
+                        
+
+                       
+                    }
+                
+                }
+
+            }
+        }
+    }
+}
---
+++ Generic_Constrains_Task3/Generic_Constrains_Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Generic_Constrains_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Belarus");
+            myDictionary.Add(5, "Russia");
+            myDictionary.Add(7, "Ukraine");
+            myDictionary.Add(3, "Poland");
+            myDictionary.Add(15, "USA");
+            myDictionary.Remove(5);
+            myDictionary.Remove(15);
+            Console.WriteLine(myDictionary[5]);
+            Console.WriteLine("Numbers of countries in dictionary {0}", myDictionary.Lenght);
+            
+        }
+    }
+}
