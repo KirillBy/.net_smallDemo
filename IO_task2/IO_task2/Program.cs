f27f4182cafb73f4b93ae0433bb5b68630d9b9ca
Kiryl Balanovich <balanovichks@gmail.com>
2020-05-05 12:14:51 +0300

    IO task 2 add

---
+++ IO_task2/IO_task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace IO_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var file2 = new FileStream("test.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            var writer = new StreamWriter(file2);
+            writer.WriteLine("Stay home \nStay true");
+
+
+            
+            var reader = new StreamReader(file2);
+            
+            Console.WriteLine(reader.ReadToEnd());
+            writer.Close();
+            file2.Close();
+           
+     
+        }
+    }
+}
