fe9a591d28016bcdfe448b73b826b23e04a2ceb5
Kirill <balanovichks@gmail.com>
2021-01-13 16:22:39 +0300

    2.2 2.3 done

---
+++ Cuncurrency/2.2AsyncBasis_ReturningOfFinishedTasks/Program.cs
@@ -0,0 +1,73 @@
+﻿//Task: Realize synchronys method with asynchronys signature
+//Usefull with module testing when stub or imitation is nessesary
+//for asynchronys interface
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _2._2AsyncBasis_ReturningOfFinishedTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new MySynchronousImplementation();
+            var result = test.GetValueAsync();
+            Console.WriteLine(result.Result);
+
+            var result2 = test.DoSomethingAsync();
+            Console.WriteLine(result2.Status);
+        }
+    }
+
+    interface IMyAsyncInterface
+    {
+        //If Task has a returning value
+        Task<int> GetValueAsync();
+
+        //If Task has no returning value
+        Task DoSomethingAsync();
+
+        //If Task with cancellation token
+        Task<int> GetValueAsync(CancellationToken cancellationToken);
+    }
+
+    class MySynchronousImplementation : IMyAsyncInterface
+    {
+        private void DoSomethingSynchronously()
+        {
+            throw new Exception();
+        }
+
+        public Task DoSomethingAsync()
+        {
+            //If Synchronous method not throw exception task.completedTask
+            try
+            {
+                DoSomethingSynchronously();
+                return Task.CompletedTask;
+            }
+            ////If Synchronous method throw exception task.fromexception
+            catch(Exception ex)
+            {
+                return Task.FromException(ex);
+            }
+            
+        }
+
+        public Task<int> GetValueAsync()
+        {
+            return Task.FromResult(13);
+        }
+
+        public Task<int> GetValueAsync(CancellationToken cancellationToken)
+        {
+            if(cancellationToken.IsCancellationRequested)
+            {
+                return Task.FromCanceled<int>(cancellationToken);
+            }
+
+            return Task.FromResult(13);
+        }
+    }
+}
---
+++ Cuncurrency/2.3AsyncBasis_TransferInformationAboutOperationProccess/Program.cs
@@ -0,0 +1,41 @@
+﻿//Task: need to react to operation progress
+using System;
+using System.Threading.Tasks;
+
+namespace _2._3AsyncBasis_TransferInformationAboutOperationProccess
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await CallMyMethodAsync();
+        }
+
+        static async Task MyMethodAsync(IProgress<double> progress = null)
+        {
+            bool done = false;
+            double percentComplete = 0;
+            while(!done)
+            {
+                if(percentComplete == 10)
+                {
+                    done = true;
+                }
+                percentComplete++;
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                progress?.Report(percentComplete);
+            }
+        }
+
+        static async Task CallMyMethodAsync()
+        {
+            var progress = new Progress<double>();
+            progress.ProgressChanged += (sender, args) => 
+            {
+                Console.WriteLine(args.ToString());
+            };
+            
+            await MyMethodAsync(progress);
+        }
+    }
+}
