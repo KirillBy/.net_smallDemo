111314d9c16fad1674397df4c15deac038a0ea89
Kirill <balanovichks@gmail.com>
2021-10-20 14:17:00 +0300

    Singleton done

---
+++ Patterns/Singleton_ChoclateBoiler/ChocolateBoiler.cs
@@ -0,0 +1,48 @@
+﻿namespace Singleton_ChoclateBoiler
+{
+    public class ChocolateBoiler
+    {
+        public bool IsEmpty { get; private set; }
+        public bool IsBoiled { get; private set; }
+        private static ChocolateBoiler _boiler;
+        static object locker = new object();
+
+        private ChocolateBoiler()
+        {
+            IsBoiled = false;
+            IsEmpty = false;
+        }
+
+        public static ChocolateBoiler getChocolateBoiler()
+        {
+            lock (locker)
+            {
+                if (_boiler == null)
+                    _boiler = new ChocolateBoiler();   
+            }
+
+            return _boiler;
+        }
+
+        public void fill()
+        {
+            if (IsBoiled)
+            {
+                IsEmpty = false;
+                IsBoiled = false;
+            }
+        }
+
+        public void boil()
+        {
+            if (!IsEmpty && !IsBoiled)
+                IsBoiled = true;
+        }
+
+        public void drain()
+        {
+            if (!IsEmpty && IsBoiled)
+                IsEmpty = true;
+        }
+    }
+}
---
+++ Patterns/Singleton_ChoclateBoiler/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton_ChoclateBoiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var boiler1 = ChocolateBoiler.getChocolateBoiler();
+            Console.WriteLine(boiler1.IsBoiled);
+            boiler1.boil();
+            var boiler2 = ChocolateBoiler.getChocolateBoiler();
+            Console.WriteLine(boiler1.IsBoiled);
+            Console.WriteLine(boiler2.IsBoiled);
+            Console.WriteLine(Object.ReferenceEquals(boiler1, boiler2));
+        }
+    }
+}
