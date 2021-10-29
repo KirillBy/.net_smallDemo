7c5a166c56e237514dca9d5b0bfd1b3c1cdb9f9c
Kirill <balanovichks@gmail.com>
2021-10-29 17:37:07 +0300

    TemplateMethod done

---
+++ Patterns/TemplateMethod_TeaCoffee/HotSpiritBeverage.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+
+namespace TemplateMethod_TeaCoffee
+{
+    public abstract class HotSpiritBeverage
+    {
+        public void PrepareRecipe()
+        {
+            BoilWater();
+            Brew();
+            PourInCup();
+            AddCondiments();
+            AddSugar();
+        }
+
+        protected void BoilWater()
+        {
+            Console.WriteLine("Water is boiling");
+        }
+
+        protected void PourInCup()
+        {
+            Console.WriteLine("Pouring hot spirit into cup");
+        }
+
+        protected abstract void Brew();
+        protected abstract void AddCondiments();
+
+        protected virtual void AddSugar()
+        {
+            Console.WriteLine("Adding two spoons of sugar");
+        }
+    }
+    
+    public class Tea : HotSpiritBeverage
+    {
+        protected override void Brew()
+        {
+            Console.WriteLine("Steeping the tea");
+        }
+
+        protected override void AddCondiments()
+        {
+            Console.WriteLine("Adding Lemon");
+        }
+
+        protected override void AddSugar()
+        {
+            Console.WriteLine("No sugar in tea");
+        }
+    }
+    
+    public class Coffee : HotSpiritBeverage
+    {
+        protected override void Brew()
+        {
+            Console.WriteLine("Dripping coffee throw filter");
+        }
+
+        protected override void AddCondiments()
+        {
+            Console.WriteLine("Adding milk");
+        }
+    }
+}
---
+++ Patterns/TemplateMethod_TeaCoffee/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TemplateMethod_TeaCoffee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HotSpiritBeverage tea = new Tea();
+            HotSpiritBeverage coffee = new Coffee();
+            tea.PrepareRecipe();
+            coffee.PrepareRecipe();
+        }
+    }
+}
